01f1c392ea7d3280c8fad28d74516f86b7cae759
Von Trinks <err0r142@gmail.com>
2018-09-11 15:50:32 -0700

    inital commit

--- rockpaperscissors/Program.cs
+++ rockpaperscissors/Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
 
 namespace rockpaperscissors
 {
     class Program
     {
+        private DiscordSocketClient Client;
+        private CommandService Commands;
         static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+        private async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+
+            });
+
+            Client.MessageReceived += Client_MessageRecieved;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            string Token = System.Environment.GetEnvironmentVariable("BOT_TOKEN");
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Log(LogMessage Message)
+        {
+            Console.WriteLine($"{DateTime.Now} at {Message.Source}] {Message.Message}");
+        }
+
+        private async Task Client_MessageRecieved(SocketMessage arg)
+        {
+           
+        }
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("Rock Paper Scissors Bot!", "https://discordapp.com/developers", StreamType.NotStreaming);
         }
     }
 }
