57c0c5372d5f57aac7ff9e8328255e71989c81de
Von Trinks <err0r142@gmail.com>
2018-09-11 15:08:46 -0700

    Add project files.

---
+++ rockpaperscissors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rockpaperscissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
