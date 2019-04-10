586816ad5a073f3a73e014bfde275c2bd868cd0c
synergize <err0r142@gmail.com>
2019-04-10 02:25:14 -0700

    Added objects

--- rockpaperscissors/Core/Commands/RockPaperScissorsCommands.cs
+++ rockpaperscissors/Core/Commands/RockPaperScissorsCommands.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Newtonsoft.Json;
 
 namespace rockpaperscissors.Core.Commands
 {
@@ -14,7 +16,7 @@ namespace rockpaperscissors.Core.Commands
         bool boolean = true;
 
         [Command("void")]
-        public async Task StartGame()
+        public async Task StartGame2()
         {
             await Context.Channel.SendMessageAsync("Rock, Paper, Scissor, Shoot!");
 
@@ -44,31 +46,109 @@ namespace rockpaperscissors.Core.Commands
 
             await Context.Channel.SendMessageAsync("", false, Embed.Build());
 
-            //[Command("play")]
-            //public async Task StartGame()
-            //{
-            //    Player PlayerOne = new Player(Context.User.Username);
-
-            //}
+        [Command("play")]
+        public async Task StartGame()
+        {
+            //Player PlayerOne = new Player(Context.User.Username);
 
         }
+        ///<summary>
+        /// Checks if our JSON file exists and returns true or false.
+        /// </summary>
+        public bool CodeExists()
+        {
+            return File.Exists(BaseCodesFile);
+        }
+        ///<summary>
+        /// Checks if Directory ..\netcoreapp2.2\Data exists. If it doesn't, we create the directory and a blank BaseCodes.Json file
+        /// If the directory exists but the BaseCodes file doesn't, we create a blank JSON file.
+        /// </summary>
+        public void CheckDirectory(string file)
+        {
+            if (!Directory.Exists(file))
+            {
+                //If the directory for our basecode json file doesn't exist we create it along with the json file. 
+                DirectoryInfo dir = Directory.CreateDirectory(file);
+                var CreateFile = File.Create(BaseCodesFile);
+                CreateFile.Close();
+            }
+            else if (!CodeExists())
+            {
+                //if the directory exists but the file doesn't, we create the file. 
+                var CreateFile = File.Create(BaseCodesFile);
+                CreateFile.Close();
+            }
+        }
+        ///<summary>
+        /// Returns a list of <see cref="CodeLists"/> objects from a file locate in the passed in "File" location.
+        /// </summary>
+        public List<CodeLists> ReadFromJson(string file)
+        {
+            List<CodeLists> ListOfServers = new List<CodeLists>();
+            file = File.ReadAllText(file);
+            try
+            {
+                JsonConvert.PopulateObject(file, ListOfServers);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
-        //public class Player : RockPaperScissorsCommands
-        //{
-        //    public Player(string name)
-        //    {
-        //        name = Context.User.Username;
-        //    }
+            return ListOfServers;
+        }
+        ///<summary>
+        /// Saves a passed in list of <see cref="CodeLists"/> to a JSON file named BaseCodes.
+        /// </summary>
+        public void WriteToJson(List<CodeLists> obj)
+        {
+            using (StreamWriter file = File.CreateText(BaseCodesFile))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, obj);
+            }
 
-        //    public class GameObject : Player
-        //    {
-        //        public GameObject(string name) : base(name)
-        //        {
-        //            string playerName = name;
-        //        }
+        }
+        ///<summary>
+        /// Pulls in a list of <see cref="CodeLists"/> and formats a JSON structure with data provided from the user. 
+        /// </summary>
+        public void NewServerEntry(List<CodeLists> obj)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
 
-        //    }
-        //}
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                writer.WritePropertyName("CodesList");
+                writer.WriteStartArray();
+                writer.Formatting = Formatting.Indented;
+                foreach (var x in obj)
+                {
+                    writer.WriteStartObject();
+
+                    writer.WritePropertyName("ServerID");
+                    writer.WriteValue(x.ServerID);
+
+                    writer.WritePropertyName("CodeEntry");
+                    writer.WriteRawValue(x.CodeEntry.ToString());
+
+                    writer.WritePropertyName("RoleName");
+                    writer.WriteRawValue(x.Role);
+                    writer.WriteEndObject();
+
+                }
+                writer.WriteEndArray();
+            }
+            using (StreamWriter file = File.CreateText(BaseCodesFile))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, obj);
+                int x = 0;
+            }
+        }
 
     }
+
+
+    
 }
---
+++ rockpaperscissors/Core/Data/Objects/GameObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rockpaperscissors.Core.Data.Objects
+{
+    public class GameObject
+    {
+        private ulong _PlayerOneID;
+        private ulong _PlayerTwoID;
+        private string _Input;
+        public ulong PlayerOneID
+        {
+            get { return _PlayerOneID; }
+            set { _PlayerOneID = value; }
+        }
+        public ulong PlayerTwoID
+        {
+            get { return _PlayerTwoID; }
+            set { _PlayerTwoID = value; }
+        }
+        public string Input
+        {
+            get { return _Input; }
+            set { _Input = value; }
+        }
+        public GameObject(ulong p1, ulong p2, string input)
+        {
+            _PlayerOneID = p1;
+            _PlayerTwoID = p2;
+            _Input = input;
+        }
+    }
+}
